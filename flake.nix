278b70a1a2da75e615dbf16b7621d327390753bc
lesh <lesh@sysphere.org>
2025-02-05 10:57:02 +0800

    nix env, build and deploy workflow

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A flake with Deno in the dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.deno ];
+
+          shellHook = ''
+            echo "Deno $(deno --version) is now available in your shell"
+          '';
+        };
+      });
+}
